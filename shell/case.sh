05fcf0b01fa8436d84bbe7f6cee5ff8ec679fb04
FawGamer <raphael.maria@outlook.com>
2020-02-18 15:10:51 -0300

    New Version Script

---
+++ shell/case.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Qual tipo de Instalacao voce vai fazer"
+echo "1) Server"
+echo "2) Workstation"
+echo "3) Basic and Essentials"
+read typeinstall
+
+instal(){
+case $typeinstall in 
+    
+; 
+esac
+}
