e04dff4f32e66bafe2680b01a69a011b8b386427
FawGamer <raphael.maria@outlook.com>
2020-03-26 19:28:14 -0300

    Update Content

--- shell/Classroom8_ES.sh
+++ shell/Classroom8_ES.sh
@@ -3,3 +3,24 @@
 # STDOUT = 1
 # STDERR = 2
 
+Pipeline ("|") anexa o STDOUT em outrou STDIN
+Write (">")  Redireciona o STDOUT para outro local, em 
+Append (">>") Anexa o STDOUT para outro local
+
+
+cd 
+touch Lista.txt
+echo "Existe um valor em branco aqui" > Lista.exe
+
+cat Lista.txt
+
+echo -e "banana\Abacaxi\Uva" > Lista.txt
+
+cat Lista.txt
+
+echo -e "Maça\nJabuticaba\nMorango\nCarambola\nBanana\nCarambola\nMorango\nUva" >> Lista.txt
+
+cat Lista.txt | short
+cat Lista.txt | uniq
+cat Lista.txt | shot | uniq
+cat Lista.txt | -name Banana
