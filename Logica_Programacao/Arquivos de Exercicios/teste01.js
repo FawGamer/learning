1c2c84c67e966624526af52d2af34ebd4c22700e
FawGamer <raphael.maria@outlook.com>
2020-04-30 17:22:51 -0300

    New update

---
+++ Logica_Programacao/Arquivos de Exercicios/teste01.js
@@ -0,0 +1,50 @@
+// isso é um comentario em java script
+
+/* 
+Isso
+tambem
+é 
+um 
+comentario
+*/
+
+var a = 5;
+var b = 10;
+var c = 20;
+
+var x = true;
+var z = false;
+
+var pergunta = "Qual é o seu nome?";
+var nome = prompt(pergutna);
+var saiudação = "Olá, " + nome + "!";
+alert("Olá, " + nome + "!");
+
+
+
+var a = 1000;
+var b = 1;
+
+if (a == b); {
+    var mensagem = "a é igual a b";
+    alert(mensagem); 
+
+}
+
+if (a != b) {
+    var mensagem = "a não é igual a b";
+    alert(mensagem);
+}
+
+var a = 1000;
+var b = 1;
+if (a != b){
+    var mensagem = "a não é igual a b";
+    alert(mensagem);
+}
+
+if (a == b){
+    var mensagem = "a é igual a b";
+    alert(mensagem);
+}
+
