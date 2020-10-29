c102c39d13c7158d901e7b8b7b3400adc851bc74
raphaelmaria <raphael.maria@outlook.com>
2020-10-29 18:26:05 -0300

    Upload

---
+++ Ansible/setup.CentOS_AnsibleAWX.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Empresa: XXXXXXX
+# Desenvolvedor: Raphael xxxxx xx xxxxxxxxx Maria
+# Cargo: Administrador de Redes
+# Versão 
+# Criado em xx/xx/xxxx
+# Ticket Origem:
+# Solicitante:
+# Descrição:
+# Código com comentários:
